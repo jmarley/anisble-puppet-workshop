c3efcde4e3688bedbbd55c45e0c9e0ae61368825
jason marley <jason.marley@redhat.com>
2015-06-12 11:27:16 -0400

    added some package types and notes for later

--- puppet/environments/install/modules/install_eap6/manifests/init.pp
+++ puppet/environments/install/modules/install_eap6/manifests/init.pp
@@ -1,7 +1,15 @@
 class install_eap6 {
 
-  file { '/opt/myrepo/': ensure => directory }
+  package { createrepo:  ensure => present }
 
+  package { yum-utils:  ensure => present }
+
+  package { jboss-eap-6.4:  ensure => present }
+
+  file { '/opt/myrepo/': ensure => directory }
+ 
+  notify { 'myrepo dir added': }
+ 
   file { '/opt/myrepo/jboss-eap-6.4-1.0-1.el7.x86_64.rpm':
     ensure => "file",
     source => "puppet:///modules/eap-6-cfg/rpm/jboss-eap-6.4-1.0-1.el7.x86_64.rpm",
@@ -11,4 +19,15 @@ class install_eap6 {
     ensure => "file",
     source => "puppet:///modules/eap-6-cfg/data/jboss-repo.repo",
   }
+
+# create yum repo
+
+# add new repo to yum
+
+# install jboss eap
+
+# deploy hellow world app
+
+# start eap yum
+
 }
