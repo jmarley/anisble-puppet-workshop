ae00fcc86e427d3499093a4f55c54b0282c48814
jason marley <jason.marley@redhat.com>
2015-06-15 14:06:58 -0400

    added exec commands, but no testing

--- puppet/environments/install/modules/install_eap6/manifests/init.pp
+++ puppet/environments/install/modules/install_eap6/manifests/init.pp
@@ -1,10 +1,12 @@
 class install_eap6 {
 
-  package { createrepo:  ensure => present }
+  package { "createrepo":  ensure => present }
 
-  package { yum-utils:  ensure => present }
+  package { "yum-utils":  ensure => present }
 
-  package { jboss-eap-6.4:  ensure => present }
+  package { "jboss-eap-6.4":  
+    ensure  => present,
+    require => Exec["add_jboss_repo_to_yum"]}
 
   file { '/opt/myrepo/': ensure => directory }
  
@@ -12,22 +14,37 @@ class install_eap6 {
  
   file { '/opt/myrepo/jboss-eap-6.4-1.0-1.el7.x86_64.rpm':
     ensure => "file",
-    source => "puppet:///modules/eap-6-cfg/rpm/jboss-eap-6.4-1.0-1.el7.x86_64.rpm",
+    source => "puppet:///modules/install_eap6/rpm/jboss-eap-6.4-1.0-1.el7.x86_64.rpm",
   }
 
   file { '/opt/myrepo/jboss-repo.repo':
     ensure => "file",
-    source => "puppet:///modules/eap-6-cfg/data/jboss-repo.repo",
+    source => "puppet:///modules/install_eap6/data/jboss-repo.repo",
+  }
+  
+  file { '/opt/jboss/jboss-eap-6.4/standalone/deployments/ ':
+    ensure  => "file",
+    source  => "puppet:///modules/install_eap6/bin",
+    require => Package["jboss-eap-6.4"],
   }
 
 # create yum repo
-
+  exec { "create_yum_repository":
+    command => "/usr/bin/createrepo --database /opt/myrepo",
+    require => Package["createrepo"], 
+  }
+ 
 # add new repo to yum
-
-# install jboss eap
+  exec { "add_jboss_repo_to_yum":
+    command => "/usr/bin/yum-config-manager --add-repo /opt/myrepo/jboss-repo.repo --enable",
+    require => Package["yum-utils"],
+  }
 
 # deploy hellow world app
+ 
 
 # start eap yum
-
+  service { "jboss-eap-6.4":
+    ensure => "running",
+  }
 }
