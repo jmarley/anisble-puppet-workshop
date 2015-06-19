fa2d60171681ca9003e8c934b683236f53b2f9e1
jason marley <jason.marley@redhat.com>
2015-06-19 17:24:45 -0400

    fixed name issue with disablerepo switch

--- puppet/environments/remove/modules/remove_eap6/manifests/init.pp
+++ puppet/environments/remove/modules/remove_eap6/manifests/init.pp
@@ -1,12 +1,42 @@
 class remove_eap6 {
 
+  notify { "removing rpm": }->
+  package { "jboss-eap-6.4":
+    ensure  => absent,
+    require => Service[ "jboss-eap-6.4" ],
+  }->
+  notify { "rpm removed": }
+
+  notify { "starting to remove repo dir": }->
   file { '/opt/myrepo/':
     path    => '/opt/myrepo/',
     ensure  => absent, 
     recurse => true,
     purge   => true,
     force   => true,
-  }
+    require => Exec["disable_jboss_repo_from_yum"], 
+  }->
+  notify { "myrepo dir removed": }
 
+  notify { "disabling jboss repo dir": }->
+# remove new repo to yum
+  exec { "disable_jboss_repo_from_yum":
+    command => "/usr/bin/yum-config-manager --disablerepo jboss-repo",
+    require => Package["jboss-eap-6.4"],
+  }->
+  notify { "jboss repo disabled": }
+
+# remove new repo to yum part duex
+#  yumrepo { "disable_jboss_repo_from_yum":
+#    baseurl  => "file:///opt/myrepo/jboss-repo.repo",
+#    descr    => "EAP 6 repository",
+#    enabled  => 0,
+#    gpgcheck => 0,
+#  }
+
+# stop eap yum
+  service { "jboss-eap-6.4":
+    ensure => "stopped",
+  }
 
 }
